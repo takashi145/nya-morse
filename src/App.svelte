f6148e36fa98e02650d2cee420eadb5d7b69a264
takashi145 <t.shuhei0203@gmail.com>
2025-11-02 09:07:24 +0900

    レイアウト作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 </script>
 
-<main>
-  <div class="text-3xl">aaa</div>
-</main>
+<div>
+  <header class="p-3 bg-white shadow-md flex justify-center">
+    <img src="/images/logo.png" alt="にゃーるす信号" class="w-28" />
+  </header>
+
+  <main class="min-h-screen">
+    <div class="max-w-5xl my-8 mx-auto bg-white p-4 shadow rounded-lg">a</div>
+  </main>
+
+  <footer class="bg-white p-3">
+    &copy; 2025
+    <a href="https://github.com/takashi145" class="underline">takashi145</a>
+  </footer>
+</div>
+
+<style>
+  :global(body) {
+    background-image: url("/images/background.png");
+    background-size: cover;
+    background-position: center;
+  }
+</style>
